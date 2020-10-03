b95810668475e7198bb0a63ae041abc49678a600
Rocket47 <romtswork@gmail.com>
2020-10-03 23:16:32 +0300

    feat: implement 3 functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
-
 exports.min = function min (array) {
-  return 0;
+  let min ;
+  if ( Array.isArray(array) && array.length !== 0) {
+   min = array[0];
+  for (let i = 1; i < array.length; i++) {
+    if (min > array[i]) {
+      min = array[i]
+    }
+}
+return min;
+  }
+else {
+return 0;
+}
 }
 
 exports.max = function max (array) {
+  let max;
+  if (Array.isArray(array) && array.length !== 0) {
+    max = array[0];
+  for (let i = 1; i < array.length; i++) {
+    if (max < array[i]) {
+      max = array[i]
+    }
+  }
+  return max;
+}
+else {
   return 0;
+}  
 }
 
 exports.avg = function avg (array) {
+  let sum = 0;
+  if ( Array.isArray(array) && array.length !== 0) {
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return sum / array.length;
+}
+else {
   return 0;
+}
 }
